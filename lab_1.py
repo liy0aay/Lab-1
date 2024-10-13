c475434b528d25d9865dd8d2e8b8f8dca2d5d89f
Sofia Titova <liyo0aay@gmail.com>
2024-10-13 16:13:40 +0300

    Update

--- lab_1.py
+++ lab_1.py
@@ -8,7 +8,6 @@ END = '\u001b[0m'
 
 
 
-
 def draw_flag():
 
     for x in range(12):                            #task 1, флаг Нидерландов
@@ -25,39 +24,35 @@ def draw_string(offset, distance, length):   #task 2, рисуем узор
     return 0    
         
 
-def draw_plot():
+def draw_plot():                              #task 3, график функции y = 2 * x
     width = 20  
     height = 19 
     
     graph = [["." for raws in range(width)] for columns in range(height)]  
 
-    for i in range (height):
-        graph[i][1]= "|"
-        graph[height-2][i]="-"
+    for i in range(height):
+        graph[i][1] = "|"
+        graph[height-2][i+1] = "-"
 
     graph[-2][1] = "*"      #добавила точку с координатой [0;0]
-
-    graph[-1][0]=" "        
+    graph[-1][0] = " "        
   
 
-    for i in range (-2, -12, -2):      #координаты по оси y
+    for i in range(-2, -12, -2):      #координаты по оси y
         graph[i][0] = str(-(i+2))
-    
-    for i in range (1, 11):           #координаты по оси x
-        graph[-1][i] = str (i-1)
+    for i in range(1, 11):           #координаты по оси x
+        graph[-1][i] = str(i-1)
 
 
     for x in range(1, width):
         coord_y = 2 * x
-        y = height - 1 - coord_y  # инверсия
-        
+        y = height - 1 - coord_y  # инверсия   
         if 0 <= y <= height :
             graph[y+1][x] = "*"
 
 
     for row in graph:
         print("".join(row))
-
 
 
 
@@ -78,7 +73,8 @@ def count_seq():
         count_pos += 1
     return print (
         f'Сумма чисел по модулю на четных позициях:  {chetn} {BLUE}{(round(chetn))//12*" "}{END}\n'
-        f'Сумма чисел по модулю на нечетных позициях:{nechetn} {WHITE}{(round(nechetn))//12*" "}{END}'       )
+        f'Сумма чисел по модулю на нечетных позициях:{nechetn} {WHITE}{(round(nechetn))//12*" "}{END}'   
+                )
 
 
 if __name__ == "__main__":
